5bd3484320cd600da14d1a8292b81bf03c202cb2
MRJAHID <mrjahid350@gmail.com>
2024-10-02 17:26:30 +0600

    Faq accordian fix

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 /** Mobile Menu Toggle */
-var toggleButton = document.querySelector('.toggle-menu');
-var navBar = document.querySelector('.mobile__menu-left');
-var overlay = document.querySelector('.mobile__menu-overlay');
-var closemenu = document.querySelector('.close-menu');
-
-toggleButton.addEventListener('click', function () {
-    navBar.classList.toggle('left-open');
-    overlay.classList.toggle('active');
-});
-
-closemenu.addEventListener('click', function () {
-    navBar.classList.toggle('left-open');
-    overlay.classList.toggle('active');
-});
+// var toggleButton = document.querySelector('.toggle-menu');
+// var navBar = document.querySelector('.mobile__menu-left');
+// var overlay = document.querySelector('.mobile__menu-overlay');
+// var closemenu = document.querySelector('.close-menu');
+//
+// toggleButton.addEventListener('click', function () {
+//     navBar.classList.toggle('left-open');
+//     overlay.classList.toggle('active');
+// });
+//
+// closemenu.addEventListener('click', function () {
+//     navBar.classList.toggle('left-open');
+//     overlay.classList.toggle('active');
+// });
 /** Mobile Menu Toggle */
 
 // Faq Accordian START
